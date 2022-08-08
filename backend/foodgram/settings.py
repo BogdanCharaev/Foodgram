1d188317e2008ca2a79dff5698bad7a8bcc4cf79
Bogdan <bogdancharaev@yandex.ru>
2022-08-09 00:07:43 +0300

    fx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2*cbajb77yw3d21fny&+w2j3x1$b+y(!r(t^7iit0^=e7j^e6r'
 DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 
 # Application definition
 
